dec7491355e9328fd9645e72085f1a178dc0fa95
Vivek <vivek23699@gmail.com>
2024-01-18 01:00:39 +0530

    middleware && login

--- actions/register.ts
+++ actions/register.ts
@@ -1,38 +1,37 @@
 "use server";
 
 import * as z from "zod";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 import { db } from "../lib/db";
 import { RegisterSchema } from "../schemas";
 import { getUserByEmail } from "../data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-    const validatedFields = RegisterSchema.safeParse(values);
+  const validatedFields = RegisterSchema.safeParse(values);
 
-    if(!validatedFields.success) {
-        return { error: "Invalid Fields!"};
-    }
+  if (!validatedFields.success) {
+    return { error: "Invalid Fields!" };
+  }
 
-    const { email, password, name } = validatedFields.data;
-    const hashedPassword = await bcrypt.hash(password, 10);
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    const existingUser = await getUserByEmail(email);
+  const existingUser = await getUserByEmail(email);
 
-    if(existingUser) {
-        return { error : "Email already in use!"};
-    }
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
 
-    await db.user.create({
-        data: {
-            name,
-            email,
-            password: hashedPassword,
-        },
-    });
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
 
-    //TODO: Send verification token email
+  //TODO: Send verification token email
 
-    return { success: "User created!" };
+  return { success: "User created!" };
 };
-
---
+++ routes.ts
@@ -0,0 +1,21 @@
+// An array of routes that are accessible to the public
+// This route do not require authentication
+// type {string[]}
+
+export const publicRoutes = ["/"];
+
+// An array of routes that are used for authentication
+// These routes will redirect logged in users to /settings
+// type {string[]}
+
+export const authRoutes = ["/auth/login", "/auth/register"];
+
+// The prefix for API AUthentication routes
+//Routes that start with this prefix are used for API
+//type{string}
+
+export const apiAuthPrefix = "/api/auth";
+
+//The default redirect path after logging in
+//type {string}
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
