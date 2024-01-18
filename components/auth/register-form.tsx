a20f26ce5073c06d9896dce55602e80abf5b75d9
Vivek <vivek23699@gmail.com>
2024-01-18 14:08:07 +0530

    Callbacks

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -54,7 +54,7 @@ export const RegisterForm = () => {
   return (
     <CardWrapper
       headerLabel="Create an account"
-      backButtonLabel="ALready have an account?"
+      backButtonLabel="Already have an account?"
       backButtonHref="/auth/login"
       showSocial
     >
